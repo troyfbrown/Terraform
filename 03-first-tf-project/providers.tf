8d68e6e04aeb4a846a485dc792ac27e9b1ac4cb8
Troy Brown <troyfbrown@gmail.com>
2026-02-02 18:34:15 -0500

    worked with terraform cli

--- 03-first-tf-project/providers.tf
+++ 03-first-tf-project/providers.tf
@@ -1,29 +1,18 @@
 terraform {
-    required_version = ">= 1.7.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-        random = {
-            source = "hashicorp/random"
-            version = "~> 3.0"
-        }
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "random_id" "bucket_suffix" {
-    byte_length = 6
-}
-
-resource "aws_s3_bucket" "example_bucket" {
-    bucket = "example-bucket-${random_id.bucket_suffix.hex}"
-}
-
-output "bucket_name" {
-    value = aws_s3_bucket.example_bucket.bucket
-}
