95caaa0be145bbb30a353de5a37d059d0d217185
Troy Brown <troyfbrown@gmail.com>
2026-03-13 15:36:23 -0400

    creating instances from a list

--- 11-multiple-resources/compute.tf
+++ 11-multiple-resources/compute.tf
@@ -1,3 +1,9 @@
+locals {
+  ami_ids = {
+    ubuntu = data.aws_ami.ubuntu.id
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Owner is Cannonical
@@ -18,10 +24,18 @@ resource "aws_instance" "from_count" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
-  # 0 % 2 = 0
-  # 1 % 2 = 1
-  # 2 % 2 = 0
-  # 3 % 2 = 1
+
+  tags = {
+    Name    = "${local.project}-${count.index}"
+    Project = local.project
+  }
+}
+
+resource "aws_instance" "from_list" {
+  count         = length(var.ec2_instance_config_list)
+  ami           = local.ami_ids[var.ec2_instance_config_list[count.index].ami]
+  instance_type = var.ec2_instance_config_list[count.index].instance_type
+  subnet_id     = aws_subnet.main[count.index % length(aws_subnet.main)].id
 
   tags = {
     Name    = "${local.project}-${count.index}"
--- 11-multiple-resources/terraform.tfvars
+++ 11-multiple-resources/terraform.tfvars
@@ -1,1 +1,12 @@
-ec2_instance_count = 4
+ec2_instance_count = 0
+
+ec2_instance_config_list = [
+  {
+    instance_type = "t3.micro",
+    ami           = "ubuntu"
+  },
+  {
+    instance_type = "t3.micro",
+    ami           = "ubuntu"
+  }
+]
