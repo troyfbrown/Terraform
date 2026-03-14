f4d06fe4682de039210f5b29455574b80a5e8627
Troy Brown <troyfbrown@gmail.com>
2026-03-13 22:32:38 -0400

    validating instance type and ami values

--- 11-multiple-resources/variables.tf
+++ 11-multiple-resources/variables.tf
@@ -13,4 +13,20 @@ variable "ec2_instance_config_list" {
     instance_type = string
     ami           = string
   }))
+
+  # Ensure that only t3-micro is used
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list : contains(["t3.micro"], config.instance_type)
+    ])
+    error_message = "Only t3.micro instances are allowed."
+  }
+
+  # Ensure that only ubuntu and nginx images are used
+  validation {
+    condition = alltrue([
+      for config in var.ec2_instance_config_list : contains(["nginx", "ubuntu"], config.ami)
+    ])
+    error_message = "At least one of the provided \"ami\" values is not supported. \nSupported \"ami\" values: \"ubuntu\", \"nginx\"."
+  }
 }
