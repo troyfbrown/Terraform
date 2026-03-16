bc54c24a8fe260520335fc09f0553a783813bdbe
Troy Brown <troyfbrown@gmail.com>
2026-03-16 18:32:52 -0400

    Validation for maps

--- 11-multiple-resources/variables.tf
+++ 11-multiple-resources/variables.tf
@@ -37,4 +37,20 @@ variable "ec2_instance_config_map" {
     ami           = string
     subnet_index  = optional(number)
   }))
+
+  # Ensure that only t3-micro is used
+  validation {
+    condition = alltrue([
+      for config in values(var.ec2_instance_config_map) : contains(["t3.micro"], config.instance_type)
+    ])
+    error_message = "Only t3.micro instances are allowed."
+  }
+
+  # Ensure that only ubuntu and nginx images are used
+  validation {
+    condition = alltrue([
+      for config in values(var.ec2_instance_config_map) : contains(["nginx", "ubuntu"], config.ami)
+    ])
+    error_message = "At least one of the provided \"ami\" values is not supported. \nSupported \"ami\" values: \"ubuntu\", \"nginx\"."
+  }
 }
