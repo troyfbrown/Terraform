84f1356bf1a27867002134b0bb054efa7ee1b819
Troy Brown <troyfbrown@gmail.com>
2026-03-16 21:57:14 -0400

    creating users in AWS

--- proj02-iam-users/users.tf
+++ proj02-iam-users/users.tf
@@ -2,6 +2,11 @@ locals {
   users_from_yaml = yamldecode(file("${path.module}/user-roles.yaml")).users
 }
 
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_from_yaml[*].username)
+  name     = each.value
+}
+
 output "users" {
   value = local.users_from_yaml
 }
