8cf6d03b93193728b92499466be890c985acb16a
Troy Brown <troyfbrown@gmail.com>
2026-03-19 15:42:56 -0400

    policies assigned to roles

--- proj02-iam-users/roles.tf
+++ proj02-iam-users/roles.tf
@@ -15,6 +15,15 @@ locals {
       "AmazonRDSFullAccess"
     ]
   }
+
+  role_policies_list = flatten([
+    for role, policies in local.role_policies : [
+      for policy in policies : {
+        role   = role
+        policy = policy
+      }
+    ]
+  ])
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -33,4 +42,20 @@ resource "aws_iam_role" "roles" {
 
   name               = each.key
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy" "managed_policies" {
+  for_each = toset(local.role_policies_list[*].policy)
+  arn      = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+resource "aws_iam_role_policy_attachment" "role_policy_attachment" {
+  count = length(local.role_policies_list)
+  role = aws_iam_role.roles[
+    local.role_policies_list[count.index].role
+  ].name
+  policy_arn = data.aws_iam_policy.managed_policies[
+    local.role_policies_list[count.index].policy
+  ].arn
+
 }
