f3e86bdcd5994c7b9cd93b5436d21fd03563237a
Troy Brown <troyfbrown@gmail.com>
2026-03-25 14:54:10 -0400

    creating a vpc from a public module

---
+++ 12-public-modules/networking.tf
@@ -0,0 +1,14 @@
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.3"
+
+  cidr            = "10.0.0.0/16"
+  name            = "12-public-modules"
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = ["10.0.0.0/24"]
+  public_subnets  = ["10.0.128.0/24"]
+}
