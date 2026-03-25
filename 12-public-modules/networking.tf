80192aa0294d199d7e294159db2826ad15ae9506
Troy Brown <troyfbrown@gmail.com>
2026-03-25 16:16:56 -0400

    using a public module for EC2

--- 12-public-modules/networking.tf
+++ 12-public-modules/networking.tf
@@ -1,3 +1,9 @@
+locals {
+  vpc_cidr             = "10.0.0.0/16"
+  private_subnet_cidrs = ["10.0.0.0/24"]
+  public_subnet_cidrs  = ["10.0.128.0/24"]
+}
+
 data "aws_availability_zones" "azs" {
   state = "available"
 }
@@ -6,9 +12,11 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.3"
 
-  cidr            = "10.0.0.0/16"
-  name            = "12-public-modules"
+  cidr            = local.vpc_cidr
+  name            = local.project_name
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = ["10.0.0.0/24"]
-  public_subnets  = ["10.0.128.0/24"]
+  private_subnets = local.private_subnet_cidrs
+  public_subnets  = local.public_subnet_cidrs
+
+  tags = local.common_tags
 }
