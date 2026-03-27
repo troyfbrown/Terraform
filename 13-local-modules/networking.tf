0f774fc84c84a506e133e01e873280ebec5d17bf
Troy Brown <troyfbrown@gmail.com>
2026-03-27 13:18:18 -0400

    outputs udated, License/ReadMe, Example Folder

--- 13-local-modules/networking.tf
+++ 13-local-modules/networking.tf
@@ -1,9 +1,11 @@
 module "vpc" {
   source = "./modules/networking"
+
   vpc_config = {
     cidr_block = "10.0.0.0/16"
     name       = "13-local-modules"
   }
+  
   subnet_config = {
     subnet_1 = {
       cidr_block = "10.0.0.0/24"
@@ -11,8 +13,17 @@ module "vpc" {
     }
     subnet_2 = {
       cidr_block = "10.0.1.0/24"
-      public     = true      
+      public     = true
       az         = "us-east-1b"
+    }
+    subnet_3 = {
+      cidr_block = "10.0.2.0/24"
+      public     = true
+      az         = "us-east-1c"
+    }
+    subnet_4 = {
+      cidr_block = "10.0.3.0/24"
+      az         = "us-east-1a"
     }
   }
 }
