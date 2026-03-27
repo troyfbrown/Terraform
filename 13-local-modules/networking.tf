fff347ebc7b3ec5d00634c83f71db64996c1d78a
Troy Brown <troyfbrown@gmail.com>
2026-03-27 10:57:32 -0400

    converting variables to objects

--- 13-local-modules/networking.tf
+++ 13-local-modules/networking.tf
@@ -1,4 +1,7 @@
 module "vpc" {
-  source   = "./modules/networking"
-  vpc_cidr = "10.0.0.0/16"
+  source = "./modules/networking"
+  vpc_config = {
+    cidr_block = "10.0.0.0/16"
+    name       = "13-local-modules"
+  }
 }
