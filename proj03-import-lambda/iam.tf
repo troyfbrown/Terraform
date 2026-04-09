8bff25cd958dda0cd9072843c61f257396ac4e51
Troy Brown <troyfbrown@gmail.com>
2026-04-09 14:45:55 -0400

    imported the role policy
    got rid of deprecated managed_policy_arns by importing an assume role policy

--- proj03-import-lambda/iam.tf
+++ proj03-import-lambda/iam.tf
@@ -3,18 +3,48 @@ import {
   id = "manually-created-lambda-role-uibb7hau"
 }
 
-resource "aws_iam_role" "lambda_execution_role" {
-  assume_role_policy = jsonencode({
+import {
+  to = aws_iam_policy.lambda_execution
+  id = "arn:aws:iam::629066559449:policy/service-role/AWSLambdaBasicExecutionRole-f727636a-5080-4530-89fe-9b08860f634b"
+}
+
+data "aws_iam_policy_document" "assume_lambda_execution_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_execution" {
+  name = "AWSLambdaBasicExecutionRole-f727636a-5080-4530-89fe-9b08860f634b"
+  path = "/service-role/"
+  policy = jsonencode({
     Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
+      Action   = "logs:CreateLogGroup"
+      Effect   = "Allow"
+      Resource = "arn:aws:logs:us-east-1:629066559449:*"
+      }, {
+      Action   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+      Effect   = "Allow"
+      Resource = ["arn:aws:logs:us-east-1:629066559449:log-group:/aws/lambda/manually-created-lambda:*"]
     }]
     Version = "2012-10-17"
   })
-  managed_policy_arns = ["arn:aws:iam::629066559449:policy/service-role/AWSLambdaBasicExecutionRole-f727636a-5080-4530-89fe-9b08860f634b"]
-  name                = "manually-created-lambda-role-uibb7hau"
-  path                = "/service-role/"
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  assume_role_policy = data.aws_iam_policy_document.assume_lambda_execution_role.json
+  name               = "manually-created-lambda-role-uibb7hau"
+  path               = "/service-role/"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_execution.arn
 }
