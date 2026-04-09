d3acd62ef79f952dc83c431261994b1c37969435
Troy Brown <troyfbrown@gmail.com>
2026-04-09 13:37:23 -0400

    finished importing lambda function code

--- proj03-import-lambda/lambda.tf
+++ proj03-import-lambda/lambda.tf
@@ -3,20 +3,27 @@ import {
   id = "manually-created-lambda"
 }
 
+data "archive_file" "lambda_code" {
+  type        = "zip"
+  source_file = "${path.root}/build/index.mjs"
+  output_path = "${path.root}/lambda.zip"
+}
+
 resource "aws_lambda_function" "this" {
-  description                        = "A starter AWS Lambda function."
-  filename                           = null
-  function_name                      = "manually-created-lambda"
-  handler                            = "index.handler"
-  role                               = "arn:aws:iam::629066559449:role/service-role/manually-created-lambda-role-uibb7hau"
-  runtime                            = "nodejs22.x"
-  
+  description      = "A starter AWS Lambda function."
+  filename         = "lambda.zip"
+  function_name    = "manually-created-lambda"
+  handler          = "index.handler"
+  role             = "arn:aws:iam::629066559449:role/service-role/manually-created-lambda-role-uibb7hau"
+  runtime          = "nodejs22.x"
+  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+
   tags = {
     "lambda-console:blueprint" = "hello-world"
   }
 
   logging_config {
-    log_format            = "Text"
-    log_group             = "/aws/lambda/manually-created-lambda"
+    log_format = "Text"
+    log_group  = "/aws/lambda/manually-created-lambda"
   }
 }
