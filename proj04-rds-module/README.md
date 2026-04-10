53273952643ea84c98152affd2104c767eb20fd9
Troy Brown <troyfbrown@gmail.com>
2026-04-10 13:43:43 -0400

    creating subnet id and security group id variables

--- proj04-rds-module/README.md
+++ proj04-rds-module/README.md
@@ -1,5 +1,12 @@
 1. [DONE] Understand the RDS resource and which necessary resources we need
-2. Create a module with the standard structure
-3. Implement variable validation
-4. Create the necessary resources and make sure the validation is working
-5. Create the RDS instance inside of the module
+2. [DONE] Create a module with the standard structure
+3. [DONE] Implement variable validation
+4. Implement networking validation
+  4.1 Receive subnet ids and security group ids via variables
+  4.2 For subnets:
+    4.2.1 Make sure that they are not in the default VPC
+    4.2.2 Make sure that they are private
+  4.3 For security groups:
+    4.3.1 Make sure that there are no inbound rules for IP addresses
+5. Create the necessary resources and make sure the validation is working
+6. Create the RDS instance inside of the module
--- proj04-rds-module/modules/rds/variables.tf
+++ proj04-rds-module/modules/rds/variables.tf
@@ -1,10 +1,20 @@
+########################
+# General information
+########################
+
 variable "project_name" {
   type = string
+  description = "The project name. Used to name the RDS instance and add relevant tags"
 }
+
+########################
+# DB configuration
+########################
 
 variable "instance_class" {
   type    = string
   default = "db.t3.micro"
+  description = "The instance class used to create the RDS instance. Requires a free-teir instance class."
 
   validation {
     condition     = contains(["db.t3.micro"], var.instance_class)
@@ -15,6 +25,7 @@ variable "instance_class" {
 variable "storage_size" {
   type    = number
   default = 10
+  description = "The amount of storage to allocate to the RDS instance. Should be between 5GB and 10GB"
 
   validation {
     condition     = var.storage_size >= 5 && var.storage_size <= 10
@@ -25,12 +36,17 @@ variable "storage_size" {
 variable "engine" {
   type    = string
   default = "postgres-latest"
+  description = "Which engine to use for the RDS instance. Currently only postgres is supported."
 
   validation {
     condition     = contains(["postgres-latest", "postgress-14"], var.engine)
     error_message = "DB engine must be postgres-latest or postgress-14"
   }
 }
+
+########################
+# DB credentials
+########################
 
 variable "credentials" {
   type = object({
@@ -39,6 +55,7 @@ variable "credentials" {
   })
 
   sensitive = true
+  description = "The root username and password for the RDS creation."
 
   validation {
     condition = (
@@ -55,4 +72,18 @@ variable "credentials" {
     4. Contains only the following characters a-z, A-Z, 0-9, +, _, ?, -
     EOT
   }
+}
+
+########################
+# DB network
+########################
+
+variable "subnet_ids" {
+  type = list(string)
+  description = "Subent IDs to deploy the RDS instance in."
+}
+
+variable "security_group_ids" {
+  type = list(string)
+  description = "Security group IDs to attach to the RDS instance."
 }
